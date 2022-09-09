a8a166092b6ceb32fe1d8698035fd19201027f64
fgames-dev <yeshayaz@LAPTOP-O9JMTPKV.localdomain>
2022-09-09 08:38:19 -0500

    Added command system

---
+++ src/commands.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <functional>
+
+std::vector<std::string> commandNames;
+std::vector<std::function<std::string(std::string)>> commandFuncs
+{
+	
+};
